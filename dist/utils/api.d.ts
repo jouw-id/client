ba0614b4dbb04a7f36de5c13651f3395aa149b0e
lennartdeknikker <lennartdeknikker@gmail.com>
2025-02-20 14:16:57 +0100

    add tsup

--- dist/utils/api.d.ts
+++
@@ -1,4 +0,0 @@
-import { PodSchemaEntry } from "./podschema";
-export declare const readResource: (pod: string, resourceId: PodSchemaEntry) => Promise<unknown>;
-export declare const writeResource: (pod: string, resourceId: PodSchemaEntry, content: string, contentType?: string) => Promise<Blob & import("@inrupt/solid-client").WithResourceInfo>;
-export declare const deleteResource: (pod: string, resourceId: PodSchemaEntry) => Promise<boolean>;
--- dist/utils/storage.d.ts
+++
@@ -1,7 +0,0 @@
-declare const storage: {
-    get<T>(key: string, defaultValue: T): any;
-    set<T>(key: string, value: T): void;
-    remove(key: string): void;
-    isAvailable(): boolean;
-};
-export default storage;
